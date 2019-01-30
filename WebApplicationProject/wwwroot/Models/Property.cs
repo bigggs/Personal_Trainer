c8ad75f090b03c1f2b5ac8e9024577b734c080d4
Leon Davies <21707165@bucks.ac.uk>
2019-01-30 13:14:49 +0000

    commit revert

---
+++ WebApplicationProject/wwwroot/Models/Property.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationProject.Models
+{
+    public class Property
+    {
+        public int ID { get; set; }
+        public string HouseNum { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Property 360 Image")]
+        public string MainImagePath { get; set; }
+
+        [NotMappedAttribute]
+        public IFormFile MainImage { get; set; }
+
+        public ICollection<Room> Rooms { get; set; }
+
+    }
+}
